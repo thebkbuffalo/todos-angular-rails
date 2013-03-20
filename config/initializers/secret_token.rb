06419ea45dc02737873b9b77adb0629bba907e49
Klaus Fleerkötter <kaeff@gmx.net>
2013-03-20 13:07:36 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TodosAngularRails::Application.config.secret_token = 'cf301c8a27e549680966b38182e70e4c591affd8f52c8e49f9a2083250d6a76a033d3cf2e984dec882569ba8ac724e0649f617d2d753de7051f0d700d046084c'
